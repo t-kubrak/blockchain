9ec204c52553ad276f041368ac557ccfba4c4d3c
Taras <chikashikaari@gmail.com>
2018-02-25 11:54:46 -0500

    FIx server start

--- blockchain.py
+++ blockchain.py
@@ -235,11 +235,6 @@ def full_chain():
     }
     return jsonify(response), 200
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
 @app.route('/nodes/register', methods=['POST'])
 def register_nodes():
     values = request.get_json()
@@ -274,3 +269,7 @@ def consensus():
         }
 
     return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
